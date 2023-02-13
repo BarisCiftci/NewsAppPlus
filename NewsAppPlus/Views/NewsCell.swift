9c19325ca4e6d14c2a6f3f7d6e11bd6e7907ca30
Baris Ciftci <barishciftci@gmail.com>
2023-02-13 22:38:36 +0100

    Bookmark Button has been added

--- NewsAppPlus/Views/NewsCell.swift
+++ NewsAppPlus/Views/NewsCell.swift
@@ -13,16 +13,38 @@ struct NewsCell: View {
     let image: Image
     
     @State private var isPresented: Bool = false
+    @State private var bookmarkButtonPressed: Bool = false
     
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
-            image
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: UIScreen.main.bounds.width - 30, height: 200, alignment: .center)
-                .cornerRadius(20)
-            Text("Author: \(news.author)")
-                .font(.subheadline)
+            
+                
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: UIScreen.main.bounds.width - 30, height: 200, alignment: .center)
+                    .cornerRadius(20)
+            
+                    .overlay(
+                        Button(action: {
+                            bookmarkButtonPressed.toggle()
+                        }, label: {
+                            HStack {
+                                Image(systemName: "bookmark.fill")
+                                    .foregroundColor(bookmarkButtonPressed ? Color.green : Color.gray)
+                                    .font(.system(size: 48))
+                                    .font(.body)
+                                    .padding(.horizontal, 30)
+                                    .offset(y: -8)
+                                .shadow(radius: 2, x: 4 , y: 4)
+                            }
+                        }), alignment: .topLeading
+                    )
+                
+             
+                Text("Author: \(news.author)")
+                    .font(.subheadline)
+            
             
             Text(news.title)
                 .font(.title)
--- NewsAppPlus/Views/NewsListView.swift
+++ NewsAppPlus/Views/NewsListView.swift
@@ -11,12 +11,13 @@ struct NewsListView: View {
     
     let newsCollection: [NewsViewModel]
     let imageData: [String: Data]
-    
     var body: some View {
         List(self.newsCollection, id: \.url) {
             news in
-            NewsCell(news: news, image: Image(uiImage: self.imageData[news.urlToImage] == nil ? UIImage(systemName: "doc.append")! : UIImage(data: self.imageData[news.urlToImage]!)!))
+            NewsCell(news: news, image: Image(uiImage: self.imageData[news.urlToImage] == nil ? UIImage(systemName: "photo.artframe")! : UIImage(data: self.imageData[news.urlToImage]!)!))
         }
+        
         .listStyle(.plain)
+        
     }
 }
