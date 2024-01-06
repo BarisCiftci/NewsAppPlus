ed806dc3900bfa7d31a8afd90e301f1fa96b0dd4
BarisCiftci <barishciftci@gmail.com>
2024-01-06 20:02:31 +0100

    [NO_TICKET] Organize sub-views for improved code readability

--- NewsAppPlus/Views/NewsView.swift
+++ NewsAppPlus/Views/NewsView.swift
@@ -33,11 +33,41 @@ struct NewsView: View {
     }
 }
 
-#Preview {
-    NewsView()
+private struct ArticleDetails: View {
+    var newArticle: Article
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            // Display article image
+            let imageUrl = newArticle.urlToImage ?? "https://media.discordapp.net/attachments/989268324884049960/1061765655889719326/6AC54581-051B-4930-B145-B38B37DAA3DF_1_105_c.jpeg"
+            ArticleImage(newImageUrl: imageUrl, newArticleUrl: newArticle.url)
+            
+            // Display article content
+            ArticleContent(newArticle: newArticle)
+        }
+    }
 }
 
-struct ArticleContent: View {
+private struct ArticleImage: View {
+    var newImageUrl: String
+    var newArticleUrl: String
+    
+    var body: some View {
+        ZStack {
+            NavigationLink("", destination: NewsWebView(urlString: newArticleUrl))
+            AsyncImage(url: URL(string: newImageUrl)) { image in
+                image.resizable()
+            }
+        placeholder: {
+            Image(systemName: "photo.artframe")
+                .resizable()
+        }.frame(height: 200)
+            
+        }
+    }
+}
+
+private struct ArticleContent: View {
     var newArticle: Article
     
     var body: some View {
@@ -59,36 +89,9 @@ struct ArticleContent: View {
     }
 }
 
-struct ArticleImage: View {
-    var newImageUrl: String
-    var newArticleUrl: String
-    
-    var body: some View {
-        ZStack {
-            NavigationLink("", destination: NewsWebView(urlString: newArticleUrl))
-            AsyncImage(url: URL(string: newImageUrl)) { image in
-                image.resizable()
-            }
-        placeholder: {
-            Image(systemName: "photo.artframe")
-                .resizable()
-        }.frame(height: 200)
-            
-        }
-    }
-}
 
-struct ArticleDetails: View {
-    var newArticle: Article
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            // Display article image
-            let imageUrl = newArticle.urlToImage ?? "https://media.discordapp.net/attachments/989268324884049960/1061765655889719326/6AC54581-051B-4930-B145-B38B37DAA3DF_1_105_c.jpeg"
-            ArticleImage(newImageUrl: imageUrl, newArticleUrl: newArticle.url)
-            
-            // Display article content
-            ArticleContent(newArticle: newArticle)
-        }
-    }
+
+
+#Preview {
+    NewsView()
 }
