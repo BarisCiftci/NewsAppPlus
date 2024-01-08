b3f733487da247868f0f114851cdcde4d5adf069
BarisCiftci <barishciftci@gmail.com>
2024-01-08 20:18:45 +0100

    [NO_TICKET] Update the date in the expired API link

--- NewsAppPlus/Config/UrlManager.swift
+++ NewsAppPlus/Config/UrlManager.swift
@@ -10,7 +10,7 @@ import Foundation
 class UrlManager {
     private let baseUrl = "https://newsapi.org/"
     private let topHeadlinesUrl = "v2/top-headlines?"
-    private let categoryUrl = "q=apple&from=2023-12-07&to=2023-12-07&sortBy=popularity&apiKey="
+    private let categoryUrl = "q=apple&from=2023-12-24&to=2023-12-31&sortBy=popularity&apiKey="
     private let key = "e7c00742a71b450b9403d52090fc70ce"
     
     func getUrl() -> URL {
