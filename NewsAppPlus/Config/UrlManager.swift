8057053904fdf481335c05ff8499c848c40284a9
BarisCiftci <barishciftci@gmail.com>
2024-01-19 22:09:00 +0100

    Create dateFrom and dateTo in UrlManager to manage dates of news

--- NewsAppPlus/Config/UrlManager.swift
+++ NewsAppPlus/Config/UrlManager.swift
@@ -11,10 +11,12 @@ class UrlManager {
     private let baseUrl = "https://newsapi.org/"
     private let topHeadlinesUrl = "v2/top-headlines?q"
     private let key = "e7c00742a71b450b9403d52090fc70ce"
+    private let dateFrom = "2024-01-01"
+    private let dateTo = "2024-01-19"
     
     func getUrlForCategory(category: Category) -> URL {
         let categoryName = category.name().lowercased()
-        let categoryUrl = String("=\(categoryName)&from=2023-12-24&to=2023-12-31&sortBy=popularity&apiKey=")
+        let categoryUrl = String("=\(categoryName)&from=\(dateFrom)&to=\(dateTo)&sortBy=popularity&apiKey=")
         let generatedUrl = baseUrl+topHeadlinesUrl+categoryUrl+key
         return URL(string: generatedUrl)!
     }
