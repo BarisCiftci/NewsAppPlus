b6b42a5743a647c023a997d7015fc562b9e5afab
BarisCiftci <barishciftci@gmail.com>
2024-01-28 23:35:59 +0100

    Add an Alert and remove unused code
    Add an Alert to inform user that there is news data for chosen source and remove unused code

--- NewsAppPlus/Views/NewsView.swift
+++ NewsAppPlus/Views/NewsView.swift
@@ -13,55 +13,65 @@ struct NewsView: View {
     let selectedSource: String
     @State private var currentSlideIndex: Int = 0
     let maxSlides = 2
-    @State private var isTabViewHidden = false
+    @State private var showAlert = false
     
     var body: some View {
         VStack{
-            Spacer()
-            if !isTabViewHidden {
-                TabView(selection: $currentSlideIndex) {
-                    ForEach(0..<min(newsViewModel.articles.filter { $0.source.name == selectedSource }.count, maxSlides), id: \.self) { slide in
-                        
-                        SlieNewsDetails(newArticle: newsViewModel.articles.filter { $0.source.name == selectedSource }[slide])
-                    }
-                }
-                .padding(.horizontal, 8)
-                .tabViewStyle(PageTabViewStyle())
-                .onAppear(){
-                    startTimer()
+            TabView(selection: $currentSlideIndex) {
+                ForEach(0..<min(newsViewModel.articles.filter { $0.source.name == selectedSource }.count, maxSlides), id: \.self) { slide in
+                    
+                    SlieNewsDetails(newArticle: newsViewModel.articles.filter { $0.source.name == selectedSource }[slide])
                 }
             }
-            Button(action: {isTabViewHidden.toggle()}, label: {
-                Image(systemName: isTabViewHidden ? "arrow.down" : "arrow.up")
-                    .foregroundColor(.pink)
-            })
-            NavigationView {
+            .padding(.horizontal, 8)
+            .tabViewStyle(PageTabViewStyle())
+            .onAppear(){
+                startTimer()
+            }
+        }
+        .alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("There is no news data"),
+                        message: Text("Please go back to source view"),
+                        dismissButton: .default(Text("OK"))
+                    )
+                }
+        NavigationView {
+            
+            // Display list of articles displayArticles
+            List(newsViewModel.articles.filter { $0.source.name == selectedSource }, id: \.url) { article in
                 
-                // Display list of articles displayArticles
-                List(newsViewModel.articles.filter { $0.source.name == selectedSource }, id: \.url) { article in
-                    
-                    // Display article details
-                    ArticleNewsDetails(newArticle: article)
-                    
-                }
-                .listStyle(.plain)
-                .task {
-                    newsViewModel.fetchNewsForCategory(category: Category.BUSINESS)
-                }
-                .refreshable {
-                    newsViewModel.fetchNewsForCategory(category:  Category.BUSINESS)
-                }
-                .navigationTitle(selectedSource)
-                .navigationBarTitleDisplayMode(.inline)
+                // Display article details
+                ArticleNewsDetails(newArticle: article)
+                
             }
+            .listStyle(.plain)
+            .task {
+                newsViewModel.fetchNewsForCategory(category: Category.BUSINESS)
+            }
+            .refreshable {
+                newsViewModel.fetchNewsForCategory(category:  Category.BUSINESS)
+            }
+            .navigationTitle(selectedSource)
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
     
     func startTimer() {
         Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { _ in
-            withAnimation {
-                currentSlideIndex = (currentSlideIndex + 1) % min(newsViewModel.articles.filter { $0.source.name == selectedSource }.count, maxSlides)
+            // Filter articles by the selected source
+            let filteredArticles = newsViewModel.articles.filter { $0.source.name == selectedSource }
+            
+            // Check if the filtered articles count is greater than zero
+            if !filteredArticles.isEmpty {
+                // Update currentSlideIndex with animation
+                withAnimation {
+                    currentSlideIndex = (currentSlideIndex + 1) % min(filteredArticles.count, maxSlides)
+                }
+            } else {
+                self.showAlert = true
             }
         }
     }
 }
+
